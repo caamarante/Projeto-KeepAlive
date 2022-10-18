999758c863ddc4a413ede79a7669b4b32d7f27c6
Ana Carolina <ana.amarante.pb@compasso.com.br>
2022-10-18 00:40:05 -0300

    Criando junção para limpar classes do erro

--- js/login/login-erro.js
+++ js/login/login-erro.js
@@ -4,3 +4,10 @@ function mensagemDeErro() {
     mensagemErro.classList.remove('login-esconde');
     botao.classList.add('login-formulario-botao-erro');
 }
+
+function limparErro() {
+    inputLogin.classList.remove('formulario-input__erro');
+    inputSenha.classList.remove('formulario-input__erro');
+    mensagemErro.classList.add('login-esconde');
+    botao.classList.remove('login-formulario-botao-erro');
+}
