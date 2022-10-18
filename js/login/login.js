1a17b42dba369507eff0583ad042877579849db2
Ana Carolina <ana.amarante.pb@compasso.com.br>
2022-10-18 00:39:36 -0300

    Implementando lógica para quando o localStorage estiver como logado, ajustando o codigo para funções de uma maneira que fique mais legivel

--- js/login/login.js
+++ js/login/login.js
@@ -10,17 +10,19 @@ let iconeSenha = $('.icone-senha');
 
 let mensagemErro = $('.login-erro');
 
+let verificarLogado = localStorage.getItem('logado');
 
-
+if(verificarLogado == 'logado'){
+    inputLogin.value = 'admin'
+    inputSenha.value = 'admin'
+}
 
 botao.addEventListener('click', function(event) {
     event.preventDefault();
     if(inputLogin.value == 'admin' && inputSenha.value == 'admin') {
-        inputLogin.classList.remove('formulario-input__erro');
-        inputSenha.classList.remove('formulario-input__erro');
-        mensagemErro.classList.add('login-esconde');
-        botao.classList.remove('login-formulario-botao-erro');
-        window.location.href = "home.html"
+        limparErro();
+        continuarLogado();
+        myWindow = window.open("home.html", "_self");
     } else {
         mensagemDeErro();
     }
